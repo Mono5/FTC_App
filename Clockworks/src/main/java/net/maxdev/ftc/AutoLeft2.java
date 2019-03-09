c6b787d8079b81945b8480c1fd70e0c77073bd5d
MaxDev <admin@j-max.ml>
2019-03-09 06:06:48 +0200

    Commit

--- Clockworks/src/main/java/net/maxdev/ftc/AutoLeft2.java
+++ Clockworks/src/main/java/net/maxdev/ftc/AutoLeft2.java
@@ -14,23 +14,27 @@ public class AutoLeft2 extends LinearOpMode {
     @Override
     public void runOpMode() {
         wheels = new MecanumWheels();
+        detector = new DogeGoldVision();
         wheels.init(telemetry, hardwareMap);
         detector.init(hardwareMap, telemetry);
 
         waitForStart();
         detector.enable(true);
         wheels.encoderDrive(8, 8, 8, 8, 0.6, 2);
+        sleep(750);
         if (detector.getLocation() > 200 && detector.getLocation() < 400) {
             detector.enable(false);
             wheels.encoderDrive(12, 12, 12, 12, 0.6, 2);
         } else {
             wheels.encoderDrive(24, -24, -24, 24, 0.8, 3);
+            sleep(1000);
             if (detector.getLocation() > 200 && detector.getLocation() < 400) {
                 detector.enable(false);
                 wheels.encoderDrive(12, 12, 12, 12, 0.6, 2);
             } else {
                 detector.enable(false);
                 wheels.encoderDrive(-48, 48, 48, -48, 0.8, 4);
+                wheels.encoderDrive(12, 12, 12, 12, 0.6, 2);
             }
         }
     }
